3a96ef22ea5eeee3f71c217ead03e1b1d016f990
yshenbj <132647973+yshenbj@users.noreply.github.com>
2023-11-30 17:22:06 +0800

    update

--- DavidSilver/MCTD/blackjack.py
+++ DavidSilver/MCTD/blackjack.py
@@ -1,1 +1,17 @@
 # Black Jack environmemt.
+import numpy as np 
+import random
+
+class BlackJackSingleEnv():
+    def __init__(self, agent):
+        self.agent = agent
+        self.act_space = (0, 1)
+        self.obs_space = (None, None, None)
+        self.decks = [2, 3, 4, 5, 6, 7, 8, 9] * 4 + [10] * 12 + ['a'] * 4
+        random.shuffle(self.decks)
+    
+    def step(self, act):
+        pass
+
+    def get_reward(self):
+        pass
--- DavidSilver/MDP/demo.py
+++ DavidSilver/MDP/demo.py
@@ -1,7 +1,8 @@
 import numpy as np
-
+import random
 # a = np.arange(12).reshape(2, 2, 3) + 10
 # print(np.argmax(a, axis=2)) 
 
-a = np.sign([-12, 2, 24, 0])
+a = [2, 3, 4, 5, 6, 7, 8, 9] * 4 + [10] * 12 + ['a'] * 4
+random.shuffle(a)
 print(a)
