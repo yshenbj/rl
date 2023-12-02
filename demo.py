686c51b287471f1c53dd537100a6daeb27d53e9e
yshenbj <yshenbj@connect.ust.hk>
2023-12-02 20:56:24 +0800

    update

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import itertools
 # a = np.arange(12).reshape(2, 2, 3) + 10
 # print(np.argmax(a, axis=2)) 
 
@@ -7,5 +8,8 @@ import random
 # random.shuffle(a) 
 # x = a.pop()
 
-x = (np.random.randint(3), np.random.randint(3))
-print(x)
+# x = (np.random.randint(3), np.random.randint(3))
+# print(x)
+
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(a[tuple([1,1])])
