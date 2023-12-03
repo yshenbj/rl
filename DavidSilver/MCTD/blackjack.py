20e36b1386ff5a977c3e5bdbf5c1d7fc0ce04981
yshenbj <yshenbj@connect.ust.hk>
2023-12-03 22:11:11 +0800

    update

--- DavidSilver/MCTD/blackjack.py
+++ DavidSilver/MCTD/blackjack.py
@@ -5,19 +5,46 @@ import random
 class BlackJackSingleEnv():
     def __init__(self, agent):
         self.agent = agent
+        # action space: (Stick, Twist)
         self.act_space = (0, 1)
-        self.obs_space = (None, None, None)
-        self.decks = [2, 3, 4, 5, 6, 7, 8, 9] * 4 + [10] * 12 + [-1] * 4
-        random.shuffle(self.decks)
-    
-    def step(self, act):
-        assert act in self.act_space 
-        if act == self.act_space[0]:
-            return self.decks.pop() 
-        elif act == self.act_space[1]:
-            return
+        # state space: (Dealer's showing card, Current sum, Number of ace)
+        self.obs_space = [0, 0, 0] 
+        self.hidden_card = None
+        self.decks = [2, 3, 4, 5, 6, 7, 8, 9] * 4 + [10] * 12 + [1] * 4
+        random.shuffle(self.decks) 
 
-    def get_reward(self):
-        pass 
+    def palyer_draw(self, card):
+        if card > 1: 
+            self.obs_space[1] += card
+        else:
+            self.obs_space[2] += card
 
-    def 
+
+    def solve_sum(self, ls):
+        # ls: (Current sum, Number of ace)
+        pass
+
+
+    def solve_stick(self):
+        # dealer_sum = 
+        pass
+
+
+    def play(self):
+        # Draw initial cards.
+        self.palyer_draw(self.decks.pop(0))
+        self.obs_space[0] = self.decks.pop(0)
+        self.palyer_draw(self.decks.pop(0))
+        self.hidden_card = self.decks.pop(0) 
+        # Play game.
+        while True:
+            act = self.agent.decide(self.obs_space)
+            assert act in self.act_space 
+            if act == self.act_space[0]:
+                reward = self.solve_stick()
+                break
+            elif act == self.act_space[1]:
+                self.obs_space[1] += self.decks.pop(0)
+                reward = 0 if self.solve_sum(self.obs_space[1:]) > 21 else 0
+                continue 
+        return reward
--- demo.py
+++ demo.py
@@ -4,12 +4,14 @@ import itertools
 # a = np.arange(12).reshape(2, 2, 3) + 10
 # print(np.argmax(a, axis=2)) 
 
-# a = ([2, 3, 4, 5, 6, 7, 8, 9] * 4 + [10] * 12 + ['a'] * 4) * 2
-# random.shuffle(a) 
-# x = a.pop()
+a = ([2, 3, 4, 5, 6, 7, 8, 9] * 4 + [10] * 12 + ['a'] * 4) * 2
+random.shuffle(a) 
+print(a)
+x = a.pop(0)
+print(x)
 
 # x = (np.random.randint(3), np.random.randint(3))
 # print(x)
 
-a = np.array([[1,2,3],[4,5,6],[7,8,9]])
-print(a[tuple([1,1])])
+# a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+# print(a[tuple([1,1])])
