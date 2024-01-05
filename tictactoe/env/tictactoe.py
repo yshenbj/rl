2adbee78da8ab0213fc233c62a61fe450e4670e7
yshenbj <132647973+yshenbj@users.noreply.github.com>
2024-01-05 18:30:13 +0800

    update

--- tictactoe/env/tictactoe.py
+++ tictactoe/env/tictactoe.py
@@ -15,8 +15,45 @@ class TicTacToeEnv(Env):
                 "deck": Box(-1, 1, shape=(3, 3), dtype=int)
             }
         )
-        # Action can be (x, y) which put cross or 
+        # Action can be (x, y) which put X or O. 
         self.action_space = Discrete(4)
+        self.render_mode = render_mode
+        """
+        If human-rendering is used, `self.window` will be a reference
+        to the window that we draw to. `self.clock` will be a clock that is used
+        to ensure that the environment is rendered at the correct framerate in
+        human-mode. They will remain `None` until human-mode is used for the
+        first time.     
+        """
+        self.window = None
+        self.clock = None
+        
+    def _get_obs(self):
+        return self._deck
+    
+    def _get_info(self):
+        return 
+    
+    def _render_frame(self):
+        if self.window is None and self.render_mode == "human":
+            pygame.init()
+            pygame.dispaly.init()
+            self.window = pygame.display.set_mode
+    
+    def reset(self):
+        self._deck = np.zeros((3, 3))
+        observation = self._get_obs()
+        info = self._get_info()
+        if self.render_mode == "human":
+            self._render_frame()
+        return observation, info
+    
+    
+       
         
         
+# %%
+import numpy as np
+
+print(np.zeros((3, 3)))
 # %%
