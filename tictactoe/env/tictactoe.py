02d45e87d4491b2b17fa04bb795c9a8ff2382b53
yshenbj <132647973+yshenbj@users.noreply.github.com>
2024-01-08 18:36:51 +0800

    updatee

--- tictactoe/env/tictactoe.py
+++ tictactoe/env/tictactoe.py
@@ -3,7 +3,7 @@ import numpy as np
 from gymnasium import Env
 from gymnasium.space import Dict, Box, Discrete
 
-# %%
+
 class TicTacToeEnv(Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
     
@@ -38,15 +38,43 @@ class TicTacToeEnv(Env):
         if self.window is None and self.render_mode == "human":
             pygame.init()
             pygame.dispaly.init()
-            self.window = pygame.display.set_mode
+            self.window = pygame.display.set_mode(
+                (self.window_size, self.window_size)
+            )
+        if self.clock is None and self.render_mode == "human":
+            self.clock = pygame.time.Clock()
+        canvas = pygame.Surface(
+            (self.win)
+        )
     
+    def step(self, player, location):
+        if self._deck[location] == 0:
+            self._deck[location] = player
+        val = player * 3
+        terminated = (deck.sum(axis=1) == val).any() or (deck.sum(axis=0) == val).any() or (deck.diagonal().sum() == val) or (np.fliplr(deck).diagonal().sum() == val)
+        reward = 1 if terminated else 0
+        observation = self._get_obs()
+        info = self._get_info()
+        if self.render_mode == "human":
+            self._render_frame()
+        return observation, reward, terminated, False, info
+
     def reset(self):
         self._deck = np.zeros((3, 3))
         observation = self._get_obs()
         info = self._get_info()
         if self.render_mode == "human":
             self._render_frame()
-        return observation, info
+        return observation, info 
+    
+    def render(self):
+        if self.render_mode == "rgb_array":
+            return self._render_frame()
+        
+    def close(self):
+        if self.window is not None:
+            pygame.display.quit()
+            pygame.quit()
     
     
        
@@ -55,5 +83,11 @@ class TicTacToeEnv(Env):
 # %%
 import numpy as np
 
-print(np.zeros((3, 3)))
+deck = np.zeros((3, 3))
+deck[0, 0] = 1
+deck[2, 0] = 1
+print(deck)
+(deck.sum(axis=1) == -3).any()
+(deck.sum(axis=0) == 3).any()
+print(np.fliplr(deck).diagonal().sum() == 1)
 # %%
