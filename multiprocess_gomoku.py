7c12c8d1b27b8d30f8fff400895931a1e5b84956
yshenbj <yshenbj@connect.ust.hk>
2024-08-20 09:57:31 +0800

    update

--- multiprocess_gomoku.py
+++ multiprocess_gomoku.py
@@ -331,7 +331,7 @@ def run(env, policy_value_net, epoch, num_epochs, lock):
             return
 
 
-def main(num_epochs=40000, num_parallels=12):
+def main(num_epochs=40000, num_parallels=10):
     mp.set_start_method('spawn', force=True)
     env = gym.make('games/Gomoku', max_episode_steps=169)
     policy_value_net = PolicyValueNet(lr=1e-3)
